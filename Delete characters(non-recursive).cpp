8571680278be57a4473ffac61444a99ebd91eb85
Ziwen Zhao <844766049@qq.com>
2019-04-05 16:11:29 +0800

    Two ways to delete characters from a string
    recursive or non-recursive.

---
+++ Delete characters(non-recursive).cpp
@@ -0,0 +1,50 @@
+//File:Delete a short string from along string.cpp
+//We use it to C
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+int i=0,j=0,x,k;
+/*To delete a short string from along string.*/
+void deletechar(char *str1,const char *str2);
+/*Find the short string from the long string*/
+
+int main()
+{
+    /*Input the string and the pattern*/
+    char p1[100];
+    char p2[100];
+    cout<<"Please input a string: ";
+    cin.getline(p1,100,'\n');
+    cout<<"Please input a pattern: ";
+    cin.getline(p2,100,'\n');
+    /*To delete a short string from along string.*/
+    deletechar(p1,p2);
+    cout<<p1;
+
+    char c;
+    cin>>c;
+    return 0;
+}
+
+/*To delete a short string from along string.*/
+void deletechar(char *str1,const char *str2)
+{
+    char *p;
+    int i;
+    while(1)
+    {
+        /*find the position of the short string.*/
+        p=strstr(str1, str2);
+        /*Delete the short string from the long string.*/
+        if(p!=NULL){
+             for(i=strlen(str2); p[i]!='\0'; i++)
+             {
+                 p[i-strlen(str2)] = p[i];
+             }
+             p[i-strlen(str2)] = '\0';
+        }
+        else return;
+    }
+    return;
+}
